b40e7c6c65cba4d38c9c0ade9755a31fd971ff9e
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-06-29 10:28:16 -0300

    start project

---
+++ src/main/java/com/rodrigo/arquivolargurafixajobapplication/ArquivoLarguraFixaJobApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.arquivolargurafixajobapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquivoLarguraFixaJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquivoLarguraFixaJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/arquivolargurafixajobapplication/ArquivoLarguraFixaJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.arquivolargurafixajobapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquivoLarguraFixaJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
